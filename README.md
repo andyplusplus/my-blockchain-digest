36fb4f12acfdf1826aea4cc02f061412c48e6d70
andyplusplus <wuzengde@vip.sina.com>
2017-07-10 14:32:10 +0800

    update

--- README.md
+++ README.md
@@ -2,19 +2,23 @@
 awesome blockchain resources
 
 # Related Resources
-* [区块链的图谱 animation :)  :)](http://lib.csdn.net/xmt1139057136/62867/chart/%E5%8C%BA%E5%9D%97%E9%93%BE)
-* [以太坊 链私有链环境搭建（windows）](http://blog.csdn.net/u014409380/article/details/53522581) --> done
 * []()
 * []()
 
+# General Resources About Blockchain
+* [Overview of top cryptocurrencies](https://github.com/kilimchoi/cryptocurrency), [Top Value generator](http://coingenius.io/)
+* [CryptoCurrency Market Capitalizations](https://coinmarketcap.com/)
+
 # 区块链开发
+* [区块链的图谱 animation](http://lib.csdn.net/xmt1139057136/62867/chart/%E5%8C%BA%E5%9D%97%E9%93%BE)
+* [以太坊 链私有链环境搭建（windows）](http://blog.csdn.net/u014409380/article/details/53522581) --> done
 * [区块链开发（零）如何开始学习以太坊及区块链](http://blog.csdn.net/sportshark/article/details/52351415)
 * [区块链开发（一）搭建基于以太坊的私有链环境](http://blog.csdn.net/sportshark/article/details/51855007)
 * [区块链开发（二）部署并运行第一个以太坊智能合约](http://blog.csdn.net/sportshark/article/details/52249607)
 * [区块链开发（三）编写调试第一个以太坊智能合约](http://blog.csdn.net/sportshark/article/details/52497176)
 * [区块链开发（四）区块链技术详解PPT](http://blog.csdn.net/sportshark/article/details/53196087)   [链接2](http://www.infocool.net/kb/OtherCloud/201611/227498.html)
 * [区块链开发（五）区块链ICO：互联网进化的驱动力](http://blog.csdn.net/sportshark/article/details/52754821)
-* [区块链开发（六）区块链架构与应用PPT](http://blog.csdn.net/sportshark/article/details/53364690)
+* [区块链开发（六）区块链架构与应用PPT](http://blog.csdn.net/sportshark/article/details/53364690)  [链接2](http://www.procedurego.com/article/3611.html)
 * [区块链开发（七）从某保险积分案例谈区块链应用的风险与挑战](http://blog.csdn.net/sportshark/article/details/53486151)
 * [区块链开发（八）从歪曲的比特币谈区块链应用及误区](http://blog.csdn.net/sportshark/article/details/54016835)
 * [再谈区块链在积分领域的应用](http://blog.csdn.net/sportshark/article/details/52412910)
---
+++ blockchain.mm
@@ -0,0 +1,137 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Blockchain" FOLDED="false" ID="ID_171579464" CREATED="1491556975105" MODIFIED="1499301104842" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="2.143">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="22" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="University of Sydney builds new Red Belly Blockchain technology" POSITION="right" ID="ID_861664044" CREATED="1499301145733" MODIFIED="1499301152376" LINK="http://www.zdnet.com/article/university-of-sydney-builds-new-red-belly-blockchain-technology/">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Ngee Ann Polytechnic to issue blockchain diplomas in first for Singapore" POSITION="left" ID="ID_585377183" CREATED="1499321654282" MODIFIED="1499321664676" LINK="https://www.techinasia.com/video-mark-zuckerberg-met-wife-priscilla">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="&#x5168;&#x56fd;&#x9996;&#x5bb6;&#x57fa;&#x4e8e;&#x533a;&#x5757;&#x94fe;&#x7684;&#x653f;&#x52a1;&#x670d;&#x52a1;&#x9879;&#x76ee;&#x843d;&#x5730;&#x7985;&#x57ce;" POSITION="right" ID="ID_29918288" CREATED="1499321804590" MODIFIED="1499321817861" LINK="http://bj.people.com.cn/GB/n2/2016/0725/c233086-28726425.html">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Ether.camp to award $50,000 for blockchain killer app" POSITION="left" ID="ID_1900872355" CREATED="1499323556447" MODIFIED="1499323566901" LINK="http://www.coinfox.info/news/5952-ether-camp-to-award-50-000-for-blockchain-killer-app">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Innovation: a lot of opportunities" POSITION="right" ID="ID_1848918390" CREATED="1499416375729" MODIFIED="1499416383793" LINK="https://medium.com/@jocatorres/innovation-a-lot-of-opportunities-480be0d81f68">
+<edge COLOR="#00007c"/>
+</node>
+<node POSITION="left" ID="ID_1907929047" CREATED="1499648785654" MODIFIED="1499648785781"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <span style="color: rgb(62, 62, 62); font-family: -apple-system-font, Helvetica Neue, PingFang SC, Hiragino Sans GB, Microsoft YaHei, sans-serif; font-size: 15px; font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(62, 62, 62)" face="-apple-system-font, Helvetica Neue, PingFang SC, Hiragino Sans GB, Microsoft YaHei, sans-serif" size="15px">&#25152;&#26377;&#30340;&#25191;&#34892;&#21147;&#24046;&#21644;&#25302;&#24310;&#30284;&#65292;&#24402;&#26681;&#21040;&#24213;&#26159;&#19981;&#30693;&#36947;&#33258;&#24049;</font></span>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="&#x5168;&#x7403;&#x533a;&#x5757;&#x94fe;&#x884c;&#x4e1a;&#x5168;&#x666f;&#x56fe;" POSITION="right" ID="ID_643009612" CREATED="1499649176821" MODIFIED="1499649183427" LINK="https://mp.weixin.qq.com/s?__biz=MzI3NDU3NDY3MA==&amp;mid=2247483696&amp;idx=1&amp;sn=ce5e79abdcaa40599765d30b69104248&amp;chksm=eb10bd11dc673407943a518aa587e970bfcb3b8958d387b73566368558b25a423df3a59cb7f7&amp;mpshare=1&amp;scene=1&amp;srcid=0710DaStCp8CkHvw4jXhRLTJ#rd">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="&#x533a;&#x5757;&#x94fe;&#x6740;&#x624b;&#x7ea7;&#x5e94;&#x7528;&#xff0c;&#x79bb;&#x8bde;&#x751f;&#x8fd8;&#x6709;&#x591a;&#x8fdc;" POSITION="left" ID="ID_1085201955" CREATED="1499650196743" MODIFIED="1499650204866" LINK="http://www.tmtpost.com/2669592.html">
+<edge COLOR="#007c7c"/>
+</node>
+<node TEXT="What to Do When Every New Idea is Already Taken" POSITION="right" ID="ID_703526183" CREATED="1499653795604" MODIFIED="1499653912584" LINK="https://www.entrepreneur.com/article/274711">
+<edge COLOR="#7c7c00"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Steve Jobs didn&#8217;t invent the computer, mobile digital media player or cell phone. But he made all of them a heck of a lot better, and he's known as an innovator because of it.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</map>
